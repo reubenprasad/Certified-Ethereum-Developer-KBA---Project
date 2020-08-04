81825cf533b9cb9be047412345d9ceba869df55a
reubenprasad <reubenprasad@gmail.com>
2020-08-04 16:07:13 +0530

    smart contract and testing (using productCount as pId)

--- contracts/ProductTracker.sol
+++ contracts/ProductTracker.sol
@@ -24,6 +24,14 @@ contract ProductTracker {
         address payable ownerAddress
   );
 
+  event ProductPurchased(
+        string name,
+        uint dateOfPurchase,
+        uint warrantyPeriod,
+        uint price,
+        address payable ownerAddress
+  );
+
   constructor () public {
    product_manufacturer = msg.sender;
     }
@@ -38,22 +46,40 @@ contract ProductTracker {
        _;
     }
 
-   function addProduct(uint _pId, string memory _name, uint _dateOfPurchase,uint _wPeriod, uint _price) public only_manufacturer{
+   function addProduct(string memory _name, uint _dateOfPurchase,uint _wPeriod, uint _price) public only_manufacturer{
        //Require a valid name
         require(bytes(_name).length > 0, 'name cannot be empty');
        //Require a valid price
         require(_price > 0,'price cannot be 0');
-       //Add the Product
-        product[_pId] = productDetail(_name, _dateOfPurchase, _wPeriod, _price, product_manufacturer);
        //Increment the Product Count
         productCount++;
+       //Add the Product
+        product[productCount] = productDetail(_name, _dateOfPurchase, _wPeriod, _price, product_manufacturer);
        //Emit an event when product is added
         emit ProductAdded(_name, _dateOfPurchase, _wPeriod, _price, product_manufacturer);
 
     }
 
     function buyProduct(uint _pId) public payable buy_conditions{
-        require(msg.value > product[_pId].price, "Value of sent ether is less than product price");
+        //Fetch the product
+        productDetail memory _product = product[_pId];
+        //Fetch the owner
+        address payable _seller = _product.ownerAddress;
+        //Ensure that the product has a valid id
+        require(_pId > 0 && _pId <= productCount);
+        //Ensure that buyer sends enough ether
+        require(msg.value >= _product.price);
+        //Ensure that the product has not been purchased already ------------X
+        //Transfer the ownership
+        _product.ownerAddress = msg.sender;
+        //Update the product
+        product[_pId] = _product;
+        //Transfer Ether to the seller
+        address(_seller).transfer(msg.value);
+        //Emit an event
+        emit ProductPurchased(_product.name, _product.dateOfPurchase, _product.warrantyPeriod, _product.price, msg.sender);
+
+      //  require(msg.value > product[_pId].price, "Value of sent ether is less than product price");
         
     }
 }
--- test/ProductTracker_test.js
+++ test/ProductTracker_test.js
@@ -29,11 +29,11 @@ contract('ProductTracker', ([manufacturer, buyer]) =>{
     describe('products',async() =>{         //Tests to check if products are added correctly and product count is incremented 
         let result, productCount
         before(async() =>{
-            result = await producttracker.addProduct(1001,'Samsung TV',0,2,web3.utils.toWei('1','Ether'),{from: manufacturer})   
+            result = await producttracker.addProduct('Samsung TV',0,2,web3.utils.toWei('1','Ether'),{from: manufacturer})   
             productCount = await producttracker.productCount()
         })
 
-        it('adds products', async() =>{
+        it('adds products', async() =>{     //Tests to check if products are added correctly and details are correct
             assert.equal(productCount,1)
             const event = result.logs[0].args
             assert.equal(event.name, 'Samsung TV','name is correct') 
@@ -41,11 +41,37 @@ contract('ProductTracker', ([manufacturer, buyer]) =>{
             assert.equal(event.ownerAddress,manufacturer,'owner is correct')
         })
 
-        it('lists products', async() =>{
-            const product = await producttracker.product(1001)
+        it('lists products', async() =>{    //Test to check if products are listed correctly
+            const product = await producttracker.product(productCount)
             assert.equal(product.name, 'Samsung TV','name is correct') 
             assert.equal(product.price,'1000000000000000000','price is correct')
             assert.equal(product.ownerAddress,manufacturer,'owner is correct')
+        })
+
+        it('sells products', async() =>{        //Test to check the product purchase functionality
+            //Track seller balance before purchase
+            let oldSellerBalance
+            oldSellerBalance = await web3.eth.getBalance(manufacturer)
+            oldSellerBalance = new web3.utils.BN(oldSellerBalance)
+
+            result = await producttracker.buyProduct(productCount, {from: buyer,value: web3.utils.toWei('1','Ether')})
+            const event = result.logs[0].args
+            assert.equal(event.name, 'Samsung TV','name is correct') 
+            assert.equal(event.price,'1000000000000000000','price is correct')
+            assert.equal(event.ownerAddress,buyer,'owner is correct')  //Check if owner is updated correctly
+
+            //Check that the seller received funds
+            let newSellerBalance
+            newSellerBalance = await web3.eth.getBalance(manufacturer)
+            newSellerBalance = new web3.utils.BN(newSellerBalance)
+ 
+            let price
+            price = web3.utils.toWei('1','Ether')
+            price = new web3.utils.BN(price)
+
+           const expectedBalance = oldSellerBalance.add(price)
+           assert.equal(newSellerBalance.toString(),expectedBalance.toString())
+
         })
 
     })
